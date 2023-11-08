6a39fcb7a5e1fa91ac5b70e34a7e1b9b3f618bb8
Petra Zelenakova <petra.zelenakova@gmail.com>
2023-11-08 19:21:40 +0100

    ukol6

--- ukol6.py
+++ ukol6.py
@@ -1,5 +1,5 @@
 class Auto:
-    def __int__ (self, registracni_znacka, typ_vozidla, najete_km):
+    def __init__ (self, registracni_znacka, typ_vozidla, najete_km):
         self.registracni_znacka = registracni_znacka
         self.typ_vozidla = typ_vozidla
         self.najete_km = najete_km
@@ -15,16 +15,16 @@ class Auto:
     def get_info(self):
         return f"Auto {self.typ_vozidla}, reg. značka {self.registracni_znacka}"
     
-Auto1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
-Auto2 = Auto("1P3 4747", "Skoda Octavia", 41253)
+Peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
+Skoda = Auto("1P3 4747", "Skoda Octavia", 41253)
 
 
 výber_auta = input("Jake auto si prejete pujcit?")
 
-if výber_auta == "Auto1":
-    print(Auto1.pujc_auto())
-elif výber_auta == "Auto2":
-    print(Auto2.pujc_auto())
+if výber_auta == "Peugeot":
+    print(Peugeot.pujc_auto())
+elif výber_auta == "Skoda":
+    print(Skoda.pujc_auto())
 else:
     print(f"Model '{výber_auta}' neni k dispozici.K dispozici jsou modely: Peugeot a Škoda")
 
